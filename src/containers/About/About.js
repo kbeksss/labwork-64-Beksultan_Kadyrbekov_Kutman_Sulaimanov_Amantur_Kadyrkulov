a900b3d6441d72a088ac1b5f1b964a4d0514afba
Beksultan Kadyrbekov <kbeksss@gmail.com>
2019-12-20 18:33:02 +0600

    Edited components About and Add

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -4,7 +4,13 @@ import './About.css';
 const About = () => {
     return (
         <div className='About'>
-
+            <h2>About us</h2>
+            <h3>Who are we?</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid deleniti dolor et harum magnam nobis provident quae ratione saepe?</p>
+            <h3>What do we do?</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ducimus eius odit porro veritatis, voluptatibus?</p>
+            <h3>Why we?</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores aut dignissimos ea eum iste placeat quis quisquam veniam.</p>
         </div>
     );
 };
--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -4,7 +4,15 @@ import './Contacts.css';
 const Contacts = () => {
     return (
         <div className='Contacts'>
-
+            <ul>
+                <li><h2>Amantur</h2></li>
+                <li><h2>Kutman</h2></li>
+                <li><h2>Nurbek</h2></li>
+                <li><h2>Aizada</h2></li>
+                <li><h2>kakoi nibud</h2></li>
+                <li><h2>kogo nibud</h2></li>
+                <li><h2></h2></li>
+            </ul>
         </div>
     );
 };
